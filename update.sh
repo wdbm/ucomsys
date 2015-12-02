6570cfd95d9fa0d68a1ef1be254618cdc90694f9
William Dmitri Breaden Madden <william.dmitri.breaden.madden@cern.ch>
2015-12-02 14:17:46 +0100

    add update.sh, tidy

--- update.sh
+++ update.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 # Install prerequisites.
-sudo apt-get -y install python-docopt
-sudo apt-get -y install python-xlib
-sudo apt-get -y install xphoon
-sudo apt-get -y install geany
-sudo apt-get -y install firefox
-sudo apt-get -y install ranger
-sudo apt-get -y install pcmanfm
-sudo apt-get -y install cmus
-sudo apt-get -y install xvkbd
-sudo apt-get -y install xdotool
-sudo apt-get -y install redshift
-sudo apt-get -y install python-pip
-sudo pip install pyprel
-sudo pip install shijian
-sudo pip install technicolor
+sudo apt-get -y install --only-upgrade python-docopt
+sudo apt-get -y install --only-upgrade python-xlib
+sudo apt-get -y install --only-upgrade xphoon
+sudo apt-get -y install --only-upgrade geany
+sudo apt-get -y install --only-upgrade firefox
+sudo apt-get -y install --only-upgrade ranger
+sudo apt-get -y install --only-upgrade pcmanfm
+sudo apt-get -y install --only-upgrade cmus
+sudo apt-get -y install --only-upgrade xvkbd
+sudo apt-get -y install --only-upgrade xdotool
+sudo apt-get -y install --only-upgrade redshift
+sudo apt-get -y install --only-upgrade python-pip
+sudo pip install pyprel --upgrade
+sudo pip install shijian --upgrade
+sudo pip install technicolor --upgrade
 
 # Install UCOM and UCOM-ELI.
-sudo mkdir -p /usr/share/ucom
 cd /usr/share/ucom
 sudo wget -N https://raw.githubusercontent.com/wdbm/ucom/master/ucom.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/pyrecon/master/pyrecon.py
@@ -28,8 +27,8 @@ sudo wget -N https://raw.githubusercontent.com/wdbm/monx/master/configuration_sn
 sudo wget -N https://raw.githubusercontent.com/wdbm/snap/master/snap.py
 
 # Install CERN-alias.
-cd /usr/share/ucom
-sudo git clone https://github.com/wdbm/CERN-alias.git
+cd /usr/share/ucom/CERN-alias
+sudo git pull
 
 # Set up UCOM launch script.
 IFS= read -d '' sessionScript << "EOF"
@@ -52,5 +51,5 @@ Type=Application
 EOF
 sudo bash -c "echo \"${sessionLauncher}\" > /usr/share/xsessions/UCOM.desktop"
 
-echo "setup complete"
+echo "update complete"
 echo "Restart to use UCOM."
