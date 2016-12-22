4a00700d7dfd626d527cb35da7ef063942434c6e
Will Breaden Madden <zicodian@gmail.com>
2016-12-22 03:04:45 +0000

    add UCOM-panel, add update CERN-alias to setup scripts, remove update.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 sudo apt-get -y install python-docopt
 sudo apt-get -y install python-xlib
 sudo apt-get -y install xphoon
+sudo apt-get -y install feh
 sudo apt-get -y install maximus
 sudo apt-get -y install geany
 sudo apt-get -y install firefox
@@ -20,26 +21,33 @@ sudo pip install pyprel
 sudo pip install shijian
 sudo pip install technicolor
 
-# Install UCOM and UCOM-ELI.
+# Install UCOM, UCOM-panel and UCOM-ELI.
 sudo mkdir -p /usr/share/ucom
 cd /usr/share/ucom
 sudo wget -N https://raw.githubusercontent.com/wdbm/ucom/master/ucom.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/pyrecon/master/pyrecon.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/ucom-eli/master/ucom-eli.py
+sudo wget -N https://raw.githubusercontent.com/wdbm/ucom-panel/master/ucom-panel.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/monx/master/monx.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/monx/master/configuration_snap.md
 sudo wget -N https://raw.githubusercontent.com/wdbm/snap/master/snap.py
 
-# Install CERN-alias.
+# Install or update CERN-alias.
 cd /usr/share/ucom
-sudo git clone https://github.com/wdbm/CERN-alias.git
+if [ -d "CERN-alias" ]; then
+    cd CERN-alias
+    sudo git pull
+    cd ..
+else
+    sudo git clone https://github.com/wdbm/CERN-alias.git
+fi
 
 # Set up UCOM launch script.
 IFS= read -d '' session_script << "EOF"
 #!/bin/bash
 /usr/bin/python /usr/share/ucom/ucom.py &
-/usr/bin/python /usr/share/ucom/ucom-eli.py --configuration=/usr/share/ucom/CERN-alias/configuration/configuration_ucom.md
-/usr/bin/python /usr/share/ucom/monx.py --configuration=/usr/share/ucom/configuration_snap.md
+/usr/bin/python /usr/share/ucom/ucom-eli.py --configuration=/usr/share/ucom/CERN-alias/configuration/configuration_ucom.md &
+/usr/bin/python /usr/share/ucom/monx.py --configuration=/usr/share/ucom/configuration_snap.md &
 EOF
 sudo bash -c "echo \"${session_script}\" > /usr/share/ucom/ucomsys.sh"
 sudo chmod 755 /usr/share/ucom/ucomsys.sh
--- setup_Openbox.sh
+++ setup_Openbox.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install openbox
 sudo apt-get -y install python-docopt
 sudo apt-get -y install python-xlib
 sudo apt-get -y install xphoon
+sudo apt-get -y install feh
 sudo apt-get -y install maximus
 sudo apt-get -y install geany
 sudo apt-get -y install firefox
@@ -21,24 +22,33 @@ sudo pip install pyprel
 sudo pip install shijian
 sudo pip install technicolor
 
-# Install UCOM and UCOM-ELI.
+# Install UCOM, UCOM-panel and UCOM-ELI.
 sudo mkdir -p /usr/share/ucom
 cd /usr/share/ucom
 sudo wget -N https://raw.githubusercontent.com/wdbm/ucom/master/ucom.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/pyrecon/master/pyrecon.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/ucom-eli/master/ucom-eli.py
+sudo wget -N https://raw.githubusercontent.com/wdbm/ucom-panel/master/ucom-panel.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/monx/master/monx.py
 sudo wget -N https://raw.githubusercontent.com/wdbm/monx/master/configuration_snap.md
 sudo wget -N https://raw.githubusercontent.com/wdbm/snap/master/snap.py
 
-# Install CERN-alias.
+# Install or update CERN-alias.
 cd /usr/share/ucom
-sudo git clone https://github.com/wdbm/CERN-alias.git
+if [ -d "CERN-alias" ]; then
+    cd CERN-alias
+    sudo git pull
+    cd ..
+else
+    sudo git clone https://github.com/wdbm/CERN-alias.git
+fi
 
 # Set up UCOM OpenBox startup applications.
 IFS= read -d '' startup_applications << "EOF"
-/usr/bin/python /usr/share/ucom/ucom-eli.py --configuration=/usr/share/ucom/CERN-alias/configuration/configuration_ucom.md
-#/usr/bin/python /usr/share/ucom/monx.py --configuration=/usr/share/ucom/configuration_snap.md
+feh --bg-fill /usr/share/ucom/CERN-alias/wallpaper7680_4320_CERN_ffffff_3861aa.png &
+/usr/bin/python /usr/share/ucom/ucom-panel.py &
+/usr/bin/python /usr/share/ucom/ucom-eli.py --configuration=/usr/share/ucom/CERN-alias/configuration/configuration_ucom.md &
+#/usr/bin/python /usr/share/ucom/monx.py --configuration=/usr/share/ucom/configuration_snap.md &
 EOF
 sudo bash -c "echo \"${startup_applications}\" > /etc/xdg/openbox/autostart"
 
